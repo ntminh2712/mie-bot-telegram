0e13f157bf9731c0417c9c569141aee194fdc57b
minhnt-appart <nguyen_tuan_minh@appart.co.jp>
2021-06-19 16:35:21 +0700

    update bot webhook

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({
 var server = http.createServer(app);
 var token = "1604263290:AAG0mF252-Bqp0dEdDzuQ2DoS8at4cuRrMI";
 var url = "https://api.telegram.org/bot" + token;
-var webAppCallback = "https://mie-bot-telegram.herokuapp.com/";
+var webAppCallback = "https://nongtraibot.herokuapp.com/";
 
 
 app.get('/setWebhook', (req, res) => {
@@ -32,17 +32,20 @@ app.post('/', function(req, res) {
   var data = req.body.message;
   if (data == null) {
     console.log(data.update_id)
+    console.log("ready");
     res.status(200).send("OK");
   }
   console.log(data.update_id)
   var id = data.chat.id
   console.log(id)
   var new_chat_member = data.new_chat_member
+  var message_id = data.message_id
   if (new_chat_member != null) {
     var fullname = new_chat_member.first_name + " " + new_chat_member.last_name
     var messageQuote = "Xin chào, " + fullname + " vui lòng đọc nội quy trong phần description của nhóm để tránh vi phạm bị ban khỏi nhóm nhé. \n Xin Cảm ơn!"
     if (fullname != null) {
-      sendMessage(id, messageQuote)
+      // sendMessage(id, messageQuote)
+      removeMessageAddmember(id,message_id)
     }
   }
   
@@ -52,6 +55,11 @@ app.post('/', function(req, res) {
 
 function sendMessage(id, messsage){
   var response = axios.get(url + "/sendMessage?chat_id=" + id + "&text=" + encodeURIComponent(messsage));
+  console.log(response)
+}
+
+function removeMessageAddmember(chat_id, messsage_id) {
+  var response = axios.get(url + "/deleteMessage?chat_id=" + chat_id + "&messsage_id=" + messsage_id);
   console.log(response)
 }
 
